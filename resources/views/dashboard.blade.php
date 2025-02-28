8ed0ab156dad37f0ecd762f70b52b6bd8ed833cc
HAQQULYAQIIN <ahy44708@gmail.com>
2025-02-28 13:28:02 +0700

    haqqul up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
         }
         .sidebar {
             width: 250px;
-            background: #ef1da5;
+            background: #36cab9;
             color: white;
             padding: 20px;
             height: 100vh;
