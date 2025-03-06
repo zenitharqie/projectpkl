0f730d640a15b6687b1273719f596d54c4bc1fcc
HAQQULYAQIIN <ahy44708@gmail.com>
2025-03-06 22:26:21 +0700

    nambahin env

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
 
 Route::get('/welcome', function () {
     return view('welcome');
